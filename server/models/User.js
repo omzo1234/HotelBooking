58253fc2220251c0f22858afa40dae7a41fc2f60
omzo1234 <omarthiamomar@gmail.com>
2025-09-29 12:08:39 +0000

    Backend

--- server/models/User.js
+++ server/models/User.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 
-const UserSchema = mongoose.Schema({
-     _id : {type: String, require: true},
-     username : {type: String, require: true},
-     email : {type: String, require: true},
-     image : {type: String, require: true},
-     role : {type: String, enum:["user", "hotelOwner"], default:"user"},
+const userSchema = mongoose.Schema({
+    _id : {type: String, require: true},
+    username : {type: String, require: true},
+    email : {type: String, require: true},
+    image : {type: String, require: true},
+    role : {type: String, enum:["user", "hotelOwner"], default:"user"},
+    recentSearchCities : {type: String, require: true},
 
-    
+},{timestamps: true}
+);
 
-})
+const User = mongoose.model("User", userSchema);
+
+export default User;
